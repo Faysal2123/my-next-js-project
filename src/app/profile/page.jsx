9f15d031ff164c9549f4d0c4238d7d6076eed0b2
Faysal2123 <foysal2005n1@gmail.com>
2025-01-24 06:35:49 -0800

    authentication done

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,24 +1,12 @@
-'use client'; 
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import {redirect} from "next/navigation";
 
-import { useKindeAuth } from '@kinde-oss/kinde-auth-nextjs';  
-import { useRouter } from 'next/navigation'; 
+export default async function Protected() {
+  const {isAuthenticated} = getKindeServerSession();
 
-const Profile = () => {
-  const { isAuthenticated } = useKindeAuth();  
-  const router = useRouter();  
-
-  
-  if (!isAuthenticated) {
-    router.push('/api/auth/login');
-    return <div>Redirecting...</div>;  
+  if (!(await isAuthenticated())) {
+    redirect("/api/auth/login");
   }
 
-  return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold text-center">Welcome to your profile!</h1>
-      <p className="text-lg text-center mt-4">This is a protected page, accessible only to authenticated users.</p>
-    </div>
-  );
-};
-
-export default Profile;
+  return <div className="text-5xl font-bold text-center">"Welcome to your profile!"</div>;
+}
